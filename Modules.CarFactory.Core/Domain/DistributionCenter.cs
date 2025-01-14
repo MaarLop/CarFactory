e657e65163729e573a705c57e93550bc03a8f44f
MaarLop <s.mariel.lopez1995@gmail.com>
2025-01-13 21:20:41 -0300

    Add distribution and sale classes

---
+++ Modules.CarFactory.Core/Domain/DistributionCenter.cs
@@ -0,0 +1,16 @@
+﻿namespace Modules.CarFactory.Core.Domain
+{
+    public class DistributionCenter
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+
+        public DistributionCenter(int id, string desciption, string address)
+        {
+            Id = id;
+            Description = desciption;
+            Address = address;
+        }
+    }
+}
