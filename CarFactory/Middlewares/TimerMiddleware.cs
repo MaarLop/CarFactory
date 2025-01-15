6b027eabb100d7bb75aa33e38bae6fc93b774dc7
MaarLop <s.mariel.lopez1995@gmail.com>
2025-01-15 01:04:03 -0300

    Add log with delay in ms

--- CarFactory/Middlewares/TimerMiddleware.cs
+++ CarFactory/Middlewares/TimerMiddleware.cs
@@ -3,10 +3,12 @@
     public class TimerMiddleware
     {
         private readonly RequestDelegate _next;
+        private readonly ILogger<TimerMiddleware> _logger;
 
-        public TimerMiddleware(RequestDelegate next)
+        public TimerMiddleware(RequestDelegate next, ILogger<TimerMiddleware> logger)
         {
             _next = next;
+            _logger = logger;
         }
 
         public async Task InvokeAsync(HttpContext context)
@@ -23,7 +25,7 @@
                 stopwatch.Stop();
 
                 context.Response.Headers["X-Elapsed-Time-ms"] = stopwatch.ElapsedMilliseconds.ToString();
-
+                _logger.LogInformation($"Request finish after {stopwatch.ElapsedMilliseconds.ToString()} ms");
                 memoryStream.Seek(0, SeekOrigin.Begin);
                 await memoryStream.CopyToAsync(originalBodyStream);
             }
